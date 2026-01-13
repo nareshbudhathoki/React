71271ab3a661e971d3b2e09c1656e06bc10b8896
White-Town <nareshbc5555@gmail.com>
2026-01-13 10:35:28 +0000

    finished project 2 completely

--- 2-project/src/App.jsx
+++ 2-project/src/App.jsx
@@ -1,14 +1,54 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState("olive");
   return (
-    <>
-
-    </>
-  )
+    <div
+      className="w-full h-screen duration-100"
+      style={{ backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-7 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white  px-3 py-2 rounded">
+          <button
+          onClick={() =>setColor("red")}
+            className="outline-none px-5 py-2 rounded text-white shadow-2xl"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() =>setColor("green")}
+            className="outline-none px-5 py-2 rounded text-white shadow-2xl"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() =>setColor("blue")}
+            className="outline-none px-5 py-2 rounded text-white shadow-2xl"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() =>setColor("purple")}
+            className="outline-none px-5 py-2 rounded text-white shadow-2xl"
+            style={{ backgroundColor: "purple" }}
+          >
+            purple
+          </button>
+          <button
+            onClick={() =>setColor("gray")}
+            className="outline-none px-5 py-2 rounded text-white shadow-2xl"
+            style={{ backgroundColor: "gray" }}
+          >
+            gray
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
